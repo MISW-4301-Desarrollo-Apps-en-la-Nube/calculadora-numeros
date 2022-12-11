e03e007809e734110079d68750f95dd0c38b2b13
andres.baron <andres.baron@rappi.com>
2022-12-11 18:03:33 -0500

    ms exponencial

--- exponential/view.py
+++ exponential/view.py
@@ -24,7 +24,6 @@ def Exponencial():
         return {"message": f"Elevando {numero} a la {potencia} se obtiene {resultado}" , "result": resultado}, 200
             
 
-""
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 6000))
     app.run(debug=True, host='0.0.0.0', port=port)
