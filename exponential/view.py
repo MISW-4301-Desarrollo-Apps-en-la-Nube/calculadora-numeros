9fafbac3c12a6e897dac76c17e8c715bbd4d125b
andres.baron <andres.baron@rappi.com>
2022-12-12 18:33:42 -0500

    fix

--- exponential/view.py
+++ exponential/view.py
@@ -30,5 +30,5 @@ def Health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 6000))
+    port = int(os.environ.get('PORT', 4000))
     app.run(debug=True, host='0.0.0.0', port=port)
